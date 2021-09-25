d45b4c4080e48dc93ba85cfab78c7e291551db74
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-25 18:53:49 +0600

    order component updated

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,20 @@
-import React from "react";
+import Button from "@restart/ui/esm/Button";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import "./Orders.css";
 import image1 from "./Products/Chips.png";
 
 const Orders = () => {
     const {orderId} = useParams();
-    console.log(orderId.name);
+    console.log(orderId);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/product/${orderId}`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }, [orderId])
+
+
   return (
     <div class="shopping-cart">
       {/* <!-- Title --> */}
@@ -26,18 +35,11 @@ const Orders = () => {
 
         <div class="description">
           <span>Common Projects</span>
-          {/* <span>Bball High</span>  
-          <span>White</span> */}
         </div>
 
         <div class="quantity">
-            {/* <img style={{width: "100px"}} src={image1} alt="" /> */}
-          {/* <button class="plus-btn" type="button" name="button">
-          </button> */}
           <input type="text" name="name" value="1" />
             <img src="minus.svg" alt="" />
-          {/* <button class="minus-btn" type="button" name="button">
-          </button> */}
         </div>
 
         <div class="total-price">$549</div>
@@ -56,18 +58,12 @@ const Orders = () => {
 
         <div class="description">
           <span>Maison Margiela</span>
-          {/* <span>Future Sneakers</span>
-          <span>White</span> */}
         </div>
 
         <div class="quantity">
             <img src="plus.svg" alt="" />
-          {/* <button class="plus-btn" type="button" name="button">
-          </button> */}
           <input type="text" name="name" value="1" />
             <img src="minus.svg" alt="" />
-          {/* <button class="minus-btn" type="button" name="button">
-          </button> */}
         </div>
 
         <div class="total-price">$870</div>
@@ -91,16 +87,13 @@ const Orders = () => {
 
         <div class="quantity">
             <img src="plus.svg" alt="" />
-          {/* <button class="plus-btn" type="button" name="button">
-          </button> */}
           <input type="text" name="name" value="1" />
             <img src="minus.svg" alt="" />
-          {/* <button class="minus-btn" type="button" name="button">
-          </button> */}
         </div>
 
         <div class="total-price">$349</div>
       </div>
+      <Button style={{width: "150px",marginTop: "10px",border: "none", backgroundColor: "green", marginLeft: "37.5rem"}}>Check Out</Button>
     </div>
   );
 };
